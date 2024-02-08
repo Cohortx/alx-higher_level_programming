99c48f022ea9a029c2ddf1f580e9d22a84118ad2
Cohortx <ibisomaxharrison@gmail.com>
2024-02-07 17:06:05 -0800

    python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""It define a class square."""
+
+
+class Square:
+    """It represent a square."""
+
+    def __init__(self, size):
+        """It Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
