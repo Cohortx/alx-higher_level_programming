6f755df41b3bc4b0d4fe020d118cac7136d88d44
Cohortx <ibisomaxharrison@gmail.com>
2024-02-15 03:07:30 -0800

    sorted out

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,31 +1,72 @@
 #!/usr/bin/python3
-
-"""It define a class Square."""
+""" A class square that defines by
+    (base on 3-square) """
 
 
 class Square:
-    """It represent a square."""
+    """ Define init method """
 
     def __init__(self, size=0):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
         """
+        Inisialize a new insatance of a square class
+
+        parameter:
+
+        size (int): size of square
+
+        Make check on size if it is int else raise TypeError
+
+        Also check if size is >= 0 else raise ValueError
+
+        Then fianlly pass
+        """
+
         self.size = size
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+        """
+        Getter method to retrive the size of the square
+
+        Returns:
+        int : size of the square
+        """
+
+        return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+
+        """
+        Setter method to retrieve the size of the square
+
+        size (int): size of square
+
+        Make check on size if it is int else raise TypeError
+
+        Also check if size is >= 0 else raise ValueError
+
+        Then fianlly pass
+        """
+
+        try:
+            self.__size = value
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+        finally:
+            pass
+
+    """
+    define Publlic method thats return curren square area """
 
     def area(self):
-        ""It return the current area of the square."""
-        return (self.__size * self.__size)
+
+        """
+        calculate the area of current square
+
+        return the area of square
+        """
+
+        return self.__size ** 2
